4045d3d0dcecd0c0907f7d7c5e22201cc2147525
Pablo <pablo.vazquezg@protonmail.com>
2023-07-25 15:35:44 -0600

    Limpieza de imports sin usar

--- src/collections/Places.ts
+++ src/collections/Places.ts
@@ -1,6 +1,4 @@
 import { CollectionConfig } from "payload/types";
-import _ from "lodash";
-import { logger } from "../lib/logger";
 
 export const Places: CollectionConfig = {
   slug: "places",
@@ -58,70 +56,4 @@ export const Places: CollectionConfig = {
       min: 0,
     },
   ],
-  hooks: {
-    // Logs
-    // afterChange: [
-    //   async ({ operation, req: { payload, user }, previousDoc, doc }) => {
-    //     // Create log
-    //     if (operation === "create") {
-    //       logger.create({ user, doc });
-    //     } else if (operation === "update") {
-    //       // Obtenemos la lista de campos que cambiaron y filtramos los que siguen igual
-    //       // Ejemplo: ["Name: Antes -> Después", "Description: Antes -> Después"]
-    //       const changedFields = Object.keys(doc).filter((key) => {
-    //         return (
-    //           !_.isEqual(doc[key], previousDoc[key]) && key !== "updatedAt"
-    //         );
-    //       });
-    //       // Todo: extraer lógica a una función
-    //       // Todo: eliminar campos que no han cambiado
-    //       // Update log
-    //       await payload.create({
-    //         collection: "logs",
-    //         data: {
-    //           action: `${user.name} ${user.fatherLastName} actualizó ${doc.name}`,
-    //           user: user.id,
-    //           date: new Date(),
-    //           type: "update",
-    //           coleccion: "places",
-    //           description: `Se actualizó la obra ${
-    //             doc.name
-    //           } en los siguientes campos: \n\n${changedFields
-    //             .map((field) => {
-    //               return `${field}: ${JSON.stringify(
-    //                 previousDoc[field],
-    //                 null,
-    //                 2
-    //               )} -> ${JSON.stringify(doc[field], null, 2)}`;
-    //             })
-    //             .join("\n")}
-    //                 `,
-    //         },
-    //       });
-    //     }
-    //   },
-    // ],
-    // afterDelete: [
-    //   async ({ req: { payload, user }, doc }) => {
-    //     // Delete log
-    //     await payload.create({
-    //       collection: "logs",
-    //       data: {
-    //         action: `${user.name} ${user.fatherLastName} eliminó la obra ${doc.name}`,
-    //         user: user.id,
-    //         date: new Date(),
-    //         type: "delete",
-    //         coleccion: "places",
-    //         description: `Se eliminó la obra ${
-    //           doc.name
-    //         } con la siguiente información: \n\n${JSON.stringify(
-    //           doc,
-    //           null,
-    //           2
-    //         )}`,
-    //       },
-    //     });
-    //   },
-    // ],
-  },
 };
